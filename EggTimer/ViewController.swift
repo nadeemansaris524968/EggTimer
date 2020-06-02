4937b0e6d793d08b929f40a15d118abaa272e933
Nadeem Ansari <nadeem_ansari64@outlook.com>
2020-06-02 13:03:03 -0400

    Add timer and progress bar to view controller.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,7 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
     
-
-
+    let eggTimes: [String:Double] = ["Soft": 5.0, "Medium": 7, "Hard": 12]
+    var timer: Timer? = nil
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        progressBar.progress = 0.0
+    }
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer?.invalidate()
+        guard let hardness = sender.titleLabel?.text else { return }
+        guard let totalTime = eggTimes[hardness] else { return }
+        var remainingTime = totalTime
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (Timer) in
+            self.updateProgressBar(totalTime, remainingTime)
+            if remainingTime == 0 {
+                self.timer?.invalidate()
+                self.titleLabel.text = "Done!"
+                // play alarm sound
+            }
+            remainingTime-=1
+        })
+    }
+    
+    func updateProgressBar(_ totalTime: Double, _ remainingTime: Double) {
+        progressBar.progress = Float((totalTime - remainingTime)/totalTime)
+    }
+    
+    func playAlarm() {
+        
+    }
+    
 }
